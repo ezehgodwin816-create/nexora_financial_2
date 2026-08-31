2dbba2e672352504c9d0e6b87b63bcc8727d9e6e
ezehgodwin816-create <ezehgodwin816@gmail.com>
2026-08-31 03:25:25 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://krzxqmzvzgjmolepcnbg.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_YBTYFY_vqnZkncmCclQa8w_ZKB0Cyul";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
